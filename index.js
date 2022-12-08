8918e6999b48ba7e543bee52e14a4f80cdd8a224
Daniel Viana <103967950+danielviana10@users.noreply.github.com>
2022-12-08 18:23:11 -0300

    finalizado

---
+++ index.js
@@ -0,0 +1,23 @@
+// Exercício 1
+let valor1 = 10;
+let valor2 = "10";
+let comparacaoDeValores = valor1 === valor2;
+console.log(comparacaoDeValores);
+// false, 10 é um número e "10" é uma string, considerado uma palavra.
+
+
+
+// Exercício 2
+let usuarioAltura = 1.80;
+let usuario2Altura = 1.75;
+let comparacaoDeAlturas = usuarioAltura > usuario2Altura;
+console.log(comparacaoDeAlturas)
+// true, 1.80 é mais que 1.75.
+
+
+
+// Exercício 3
+let valorUm = 10;
+let valorDois = "10";
+let comparacaoDeValores2 = valorUm !== valorDois
+console.log(comparacaoDeValores2)
